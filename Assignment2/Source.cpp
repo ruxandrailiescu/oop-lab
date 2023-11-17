f76990562ff08975e6db6f2593784743c1a176e1
Ruxandra Iliescu <iliescuruxandra23@stud.ase.ro>
2023-11-17 09:08:16 +0200

    Assignment2 solution

--- Assignment2/Source.cpp
+++ Assignment2/Source.cpp
@@ -1,9 +1,11 @@
+#include <string>
+
 // Implementati o clasa care sa lucreze cu siruri de caractere, asemeni clasei String din std.
 
 class MyString {
 
-	char* str;
-	int len;
+	char* str = nullptr;
+	int len = 0;
 
 public:
 
@@ -13,6 +15,65 @@ public:
 *	- destructor
 *	- supraincarcare operator =
 */
+
+	MyString() {
+
+	}
+
+	MyString(const char* _str, int _len) {
+
+		this->setString(_str, _len);
+	}
+
+	MyString(const MyString& s) : MyString(s.str, s.len) {
+
+	}
+
+	char* getString() {
+
+		if (this->str == nullptr) {
+			return nullptr;
+		}
+
+		char* copy = new char[this->len + 1];
+		strcpy_s(copy, this->len + 1, this->str);
+		return copy;
+
+	}
+
+	int getLen() { return this->len; }
+
+	void setString(const char* _str, int _len) {
+
+		if (this->str != nullptr) {
+			delete[] this->str;
+		}
+
+		this->str = new char[_len + 1];
+		strcpy_s(this->str, _len + 1, _str);
+		this->len = _len;
+
+	}
+
+	MyString operator=(const MyString& source) {
+
+		if (&source == this) {
+			return *this;
+		}
+
+		if (this->str != nullptr) {
+			delete[] this->str;
+		}
+		this->setString(source.str, source.len);
+
+		return source;
+
+	}
+
+	~MyString() {
+
+		delete[] this->str;
+	}
 };
 
 /*	- exemplificare in main:
@@ -20,3 +81,15 @@ public:
 *	* evidentiere apel constructor de copiere
 *	* evidentiere apel operator =
 */
+
+int main() {
+
+	MyString str1("Piata Victoriei", strlen("Piata Victoriei"));
+
+	MyString str2 = str1;
+
+	MyString str3;
+	str3 = str1;
+
+//	str3 = str3;
+}
